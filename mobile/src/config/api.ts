dae8a2f8b59f64308f1ee1006a3696839562b6bb
Oleh Yaiechnyk <oleh.yaiechnyk@artcodevision.com>
2025-09-26 13:38:57 +0300

    Changed package.json

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "trainscheduler-production.up.railway.app"
+export const API_BASE_URL = "https://trainscheduler-production.up.railway.app"
 
 export const API_ENDPOINTS = {
     LOGIN: "/auth/login",
