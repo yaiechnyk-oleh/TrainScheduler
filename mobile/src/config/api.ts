0a00b002fb1d192a13982d22a9c2724f559fd8ec
Oleh Yaiechnyk <oleh.yaiechnyk@artcodevision.com>
2025-09-26 13:03:10 +0300

    Add script for migrations on run for railway and changed config to railway api

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3000"
+export const API_BASE_URL = "trainscheduler-production.up.railway.app"
 
 export const API_ENDPOINTS = {
     LOGIN: "/auth/login",
